beddc69e41e872e5be2641e6310e6e84c95b4ed6
Juan C Lopez <juancalpz@yahoo.com>
2024-02-22 08:37:29 -0400

    Added file test_amenity.py

---
+++ models/tests/test_models/test_amenity.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Unittest for Class Amenity
+"""
+
+import unittest
+from models.amenity import Amenity
+
+
+class TestAmenity(unittest.TestCase):
+    """
+    Test case
+    """
+
+    def amenity_attributes(self):
+        """
+        Attribute initialization
+        """
+        amenity = Amenity()
+        self.assertEqual(amenity.name, "")
+
+if __name__ == '__main__':
+    unittest.main()
