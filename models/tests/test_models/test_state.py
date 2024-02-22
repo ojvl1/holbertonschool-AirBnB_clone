c608f48e711dade4a3477fb8278ea65bb8f27bb8
Juan C Lopez <juancalpz@yahoo.com>
2024-02-22 08:39:21 -0400

    Added file test_state.py

---
+++ models/tests/test_models/test_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Unittest for Class State
+"""
+
+import unittest
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    """
+    Test Case
+    """
+
+    def state_attributes(self):
+        """
+        Test attribute initialization
+        """
+        state = State()
+        self.assertEqual(state.name, "")
+
+if __name__ == '__main__':
+    unittest.main()
