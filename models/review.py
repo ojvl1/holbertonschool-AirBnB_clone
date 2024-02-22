2b9a038142f3dd1b2be92acd65cdedb5a63df63f
Juan C Lopez <juancalpz@yahoo.com>
2024-02-21 21:38:21 -0400

    Added file review.py

---
+++ models/review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Class Review inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    Class has public attributes and updates
+    FileStorage to manage serialization and
+    deserialization
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
