f6f5935ddf22b37b2ccd79a55b50f34f217cfcc6
Juan C Lopez <juancalpz@yahoo.com>
2024-02-21 21:36:55 -0400

    Added file city.py

---
+++ models/city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Class City inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    Class has public attributes and updates
+    FileStorage to manage serialization and
+    deserialization
+    """
+    state_id = ""
+    name = ""
