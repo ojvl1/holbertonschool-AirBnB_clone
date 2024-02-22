f31241b15d64f6ca4ba310ce3a721b0c359d92c4
Juan C Lopez <juancalpz@yahoo.com>
2024-02-22 08:32:05 -0400

    Updated engine __init__.py to have a dict of classes

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.review import Review
+
+allclasses = {"BaseModel": BaseModel,
+              "User": User,
+              "City": City,
+              "State": State,
+              "Place": Place,
+              "Amenity": Amenity,
+              "Review": Review}
 
 storage = FileStorage()
 storage.reload()
