6b51fc5ef4841557a080621da1afdba25e2fea4c
Juan C Lopez <juancalpz@yahoo.com>
2024-02-22 08:38:24 -0400

    Added file test_place.py

---
+++ models/tests/test_models/test_place.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Unittest for Class Place
+"""
+import unittest
+from models.place import Place
+
+
+class TestPlace(unittest.TestCase):
+    """
+    Test case
+    """
+
+    def place_attributes(self):
+        """
+        Test attribute initialization
+        """
+        place = Place()
+        self.assertEqual(place.city_id, "")
+        self.assertEqual(place.user_id, "")
+        self.assertEqual(place.name, "")
+        self.assertEqual(place.description, "")
+        self.assertEqual(place.number_rooms, 0)
+        self.assertEqual(place.number_bathrooms, 0)
+        self.assertEqual(place.max_guest, 0)
+        self.assertEqual(place.price_by_night, 0)
+        self.assertEqual(place.latitude, 0.0)
+        self.assertEqual(place.longitude, 0.0)
+        self.assertEqual(place.amenity_ids, [])
+
+if __name__ == '__main__':
+    unittest.main()
