e59cf0a740e551971356bb6f17ac46a5708c9bb3
Emmanuel Maldonado <146750306+emmanuelmaldonadomaldonado@users.noreply.github.com>
2024-02-25 19:49:18 -0400

    Updated file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
 import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
@@ -18,15 +25,25 @@ class FileStorage:
         serialized_objects = {key: obj.to_dict() for key, obj in self.__objects.items()}
         with open(self.__file_path, 'w') as f:
             json.dump(serialized_objects, f)
-
+    
     def reload(self):
-        from models.base_model import BaseModel
+        """
+        deserializes the JSON file to __objects
+        """
+        data_dict = {}
+        classes = {
+            "BaseModel": BaseModel,
+            "User": User,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Place": Place,
+            "Review": Review
+        }
         try:
-            with open(self.__file_path, "r") as file:
-                data = json.load(file)
-                for key, value in data.items():
-                    cls = BaseModel
-                    obj = cls(**value)
-                    self.__objects[key] = obj
+            with open(self.__file_path, 'r') as f:
+                data_dict = json.load(f)
+                for k, v in data_dict.items():
+                    self.__objects[k] = classes[v["__class__"]](**v)
         except FileNotFoundError:
             pass
