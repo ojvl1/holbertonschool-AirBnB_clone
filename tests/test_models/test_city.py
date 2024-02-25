68f1aed2dab9d102cab2353be2bab32933f00fa9
Juan C Lopez <142533382+juancalpz23@users.noreply.github.com>
2024-02-25 19:26:47 -0400

    Test city

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""
+'''
 Unittest for Class City
-"""
+'''
 
 import unittest
 from models.city import City
 
 class TestCity(unittest.TestCase):
-    """
+    '''
     Test Case
-    """
+    '''
 
     def citty_attributes(self):
-        """
+        '''
         Test Attribute Initialization
-        """
+        '''
 
         city = City()
         self.assertEqual(city.state_id, "")
