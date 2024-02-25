f49c2939a1971a7b53cd5c9d9e4665ed107b92e6
ojvl1 <carlosantompietri175@gmail.com>
2024-02-24 23:06:22 -0400

    init

---
+++ models/place.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Class Place inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """
+    Class has public attributes and updates
+    FileStorage to manage serialization and
+    deserialization
+    """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Class User inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    Class has public attributes and updates
+    FileStorage to manage serialization and
+    deserialization of User
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
