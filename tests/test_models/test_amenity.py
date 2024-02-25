fcdfb011ab2f4b730824f46653becb14023f46a2
Juan C Lopez <142533382+juancalpz23@users.noreply.github.com>
2024-02-25 19:17:29 -0400

    Checkers

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Unittest for Class Amenity
+    Unittest for Class Amenity
 """
 
 import unittest
@@ -9,15 +9,16 @@ from models.amenity import Amenity
 
 class TestAmenity(unittest.TestCase):
     """
-    Test case
+        Test Case
     """
 
     def amenity_attributes(self):
         """
-        Attribute initialization
+            Attribute initialization
         """
         amenity = Amenity()
         self.assertEqual(amenity.name, "")
+
 
 if __name__ == '__main__':
     unittest.main()
