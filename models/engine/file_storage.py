07112c1aea9737e9fd29ece09da91a88317099ec
Juan C Lopez <juancalpz@yahoo.com>
2024-02-25 22:14:59 -0400

    Updated file_storage.py reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,11 +21,34 @@ class FileStorage:
 
     def reload(self):
         from models.base_model import BaseModel
+        from models.user import User
+        from models.amenity import Amenity
+        from models.city import City
+        from models.place import Place
+        from models.review import Review
+        from models.state import State
+
         try:
             with open(self.__file_path, "r") as file:
                 data = json.load(file)
                 for key, value in data.items():
-                    cls = BaseModel
+                    class_name = value["__class__"]
+                    if class_name == "BaseModel":
+                        cls = BaseModel
+                    elif class_name == "User":
+                        cls = User
+                    elif class_name == "Amenity":
+                        cls = Amenity
+                    elif class_name == "City":
+                        cls = City
+                    elif class_name == "Place":
+                        cls = Place
+                    elif class_name == "Review":
+                        cls = Review
+                    elif class_name == "State":
+                        cls = State
+                    else:
+                        continue
                     obj = cls(**value)
                     self.__objects[key] = obj
         except FileNotFoundError:
