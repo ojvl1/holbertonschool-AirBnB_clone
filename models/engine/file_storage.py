8ff863e359270b6e0e0a945422e98a4b22a60d9e
Juan C Lopez <juancalpz@yahoo.com>
2024-02-27 14:28:31 -0400

    Pycodestyle documentation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,26 +1,46 @@
 #!/usr/bin/python3
-
+"""
+File Storage Module
+This module serializes instances to a JSON
+file and deserializes JSON file to instances.
+"""
 import json
 
 
 class FileStorage:
+    """
+    Serializes instances to a JSON
+    file and deserializes JSON file to instances.
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """
+        Returns the dictionary __objects.
+        """
         return self.__objects
 
     def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id.
+        """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
+        """
+        Serializes __objects to the JSON file (path: __file_path).
+        """
         serialized_objects = {key: obj.to_dict()
                               for key, obj in self.__objects.items()}
         with open(self.__file_path, 'w') as f:
             json.dump(serialized_objects, f)
 
     def reload(self):
+        """
+        Deserializes the JSON file to __objects (only if the file exists).
+        """
         from models.base_model import BaseModel
         from models.user import User
         from models.amenity import Amenity
